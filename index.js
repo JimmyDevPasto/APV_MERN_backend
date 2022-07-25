ca28c1561e4ebd840f19f7be05be476099270aa8
Jimmy <jimmydeveloperpasto@gmail.com>
2022-07-25 18:57:21 -0500

    con cors

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ app.use(express.json());
 dotenv.config(); 
 conectarDB();
 
-// const dominiosPermitidos= [process.env.FRONTEND_URL];
+const dominiosPermitidos= [process.env.FRONTEND_URL];
 
-// const corsOptions= {
-//     origin:function (origin,callback) {
-//         if (dominiosPermitidos.indexOf(origin) !==-1) {
-//             //El orignes del Request esta permitido 
-//             callback(null, true)
-//         }else {
-//             callback(new Error('No permitido por Cors'))
-//         }
-//     }
-// }
+const corsOptions= {
+    origin:function (origin,callback) {
+        if (dominiosPermitidos.indexOf(origin) !==-1) {
+            //El orignes del Request esta permitido 
+            callback(null, true)
+        }else {
+            callback(new Error('No permitido por Cors'))
+        }
+    }
+}
 
 app.use(cors(corsOptions)); 
 
