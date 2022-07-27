0da6f36e75862b81efbb2f27ea3b0256d4d2a471
Jimmy <jimmydeveloperpasto@gmail.com>
2022-07-27 01:23:08 -0500

    1

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ conectarDB();
 
 // app.use(cors(corsOptions)); 
 
-app.use(cors({
-    origin: '*'
-    }));
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
-    next();
-    });
+// app.use(cors({
+//     origin: '*'
+//     }));
+// app.use((req,res,next)=>{
+//     res.header('Access-Control-Allow-Origin','*');
+//     res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
+//     res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
+//     next();
+//     });
 
 app.use("/api/veterinarios",veterinarioRoutes);
 app.use("/api/pacientes",pacienteRoutes);
